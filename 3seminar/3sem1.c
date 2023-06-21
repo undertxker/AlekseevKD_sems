d47f9e3f625ff00f2d122a9551e705513c70d621
undertxker <129529716+undertxker@users.noreply.github.com>
2023-06-21 19:29:28 +0300

    Update 3sem1.c

--- 3seminar/3sem1.c
+++ 3seminar/3sem1.c
@@ -1,112 +1,143 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-// Функция для ввода строки, в которой реализовано изменение размера выделенной памяти
-void input_string(char *string) {
-    char input;  
-// Переменная для считывания посимвольно
-    for (int i = 0; (input = getchar()) != '\n'; ++i) {
-        string = (char *) realloc(string, sizeof(char) * (i + 1));
-        string[i] = input;
-        string[i + 1] = '\0';
+#define MAX_SIZE 10
+
+typedef struct Node node_t;
+struct Node {
+    node_t *next;
+    char   *city;
+    size_t p;
+};
+
+typedef struct {
+    node_t **l;
+    size_t msize;
+} table_t;
+
+size_t hash(char const *s)
+{
+    size_t h = 0;
+    for (int i = 0; s[i]; i++) {
+        h = h * 17 + s[i];
     }
+    return h;
 }
 
-// Функция записывает значение в expanded_string, перед этим расширяет выделенную память. Для этого передается счетчик k
-void input_value(int k, char value, char *expanded_string) {
-    expanded_string = (char *) realloc(expanded_string, sizeof(char) * (k + 1));
-    expanded_string[k] = value;
-    expanded_string[k + 1] = '\0';
+node_t *new_node(char *c, size_t p)
+{
+    node_t *n = malloc(sizeof(node_t));
+    n->next = NULL;
+    n->city = strdup(c);
+    n->p = p;
+    return n;
 }
 
-// Функция для развертки строки формата 'a-z'
-void expand(char *string, char *expanded_string) {
-    char lower_value, upper_value = 0; 
-// Переменные "от", "до"
-    int count = 0; 
-// Отвечает за количество элементов, которые необходимы для развертывания
-    int k = 0; 
-// Счетчик
-    for (int j = 0; string[j] != '\0'; ++j) {
-
-// '-' перед
-        if (string[0] == '-' && j == 0) {
-            for (j = 0; string[j] == '-'; ++j) {
-                input_value(k, string[j], expanded_string);
-                ++k;
-            }
+int add(table_t *t, char *c, size_t p)
+{
+    size_t h = hash(c) % t->msize;
+    node_t *it = t->l[h];
+    while (it) {
+        if (strcmp(it->city, c) == 0) {
+            printf("city has already been added\n");
+            return 1;
         }
-
-/* Если находим символ != '-', то записываем его в переменную upper_value,
-* предварительно присвоив lower_value старый upper_value */
-        if (string[j] != '-') {
-            lower_value = upper_value;
-            upper_value = string[j];
-            count += 1;
-        }
-
-// Если есть 2 элемента, то в expanded_string[k] записываются значения
-        if (count == 2) {
-// Если нижнее значение имеет меньшее значение, чем верхнее
-            if (lower_value < upper_value) {
-                for (char value = lower_value; value <= upper_value; ++value) {
-                    input_value(k, value, expanded_string);
-                    ++k;
-                }
-                count = 0;
-            }
-// Если нижнее значение имеет большее значение, чем верхнее
-            else {
-                for (char value = lower_value; value >= upper_value; --value) {
-                    input_value(k, value, expanded_string);
-                    ++k;
-                }
-                count = 0;
-            }
-
-/* После записи значений в expanded_string проверяем 2 следующих символа в string.
-* Если оба '-', то выходим из цикла. Перед выходом дополняем строку символами '-' */
-            if (string[j + 1] == '-' && (string[j + 2] == '-' || string[j + 2] == '\0')) {
-                for (int i = j + 1; string[i] != '\0'; ++i) {
-                    input_value(k, '-', expanded_string);
-                    ++k;
-                }
-                break;
-            }
-
-/* После того, как мы записали значения в expanded_string, проверили 2 следующих символа в string.
-* Если '-', то 1) отнимаем от j единицу: таким образом впоследствии мы находим новое
-* нижнее значение. 2) отнимаем от k единицу, чтобы лишний раз <C> (нижнее значение) не выводить:
-* (-a-c-f- -> abc<C>def) */
-            if (string[j + 1] == '-') {
-                j -= 1;
-                k -= 1;
-            }
-        }
+        it = it->next;
     }
-}
-
-int task1() {
-
-    // Создание указателей на динамические массивы, выделение памяти
-    char *string1;
-    string1 = (char *) malloc(sizeof(char) * 1);
-    char *string2;
-    string2 = (char *) malloc(sizeof(char) * 1);
-
-
-    puts("Input string. (such as '-a-z-'/'a-z')");
-
-    input_string(string1);
-
-    expand(string1, string2);
-
-    printf("%s", string2);
-
-
-    // Освобождаем память
-    free(string1);
-    free(string2);
-
+    it = new_node(c, p);
+    it->next = t->l[h];
+    t->l[h] = it;
     return 0;
 }
+
+void erase(table_t *t, char const *s)
+{
+    size_t h = hash(s) % t->msize;
+    _Bool found = 0;
+    node_t *it = t->l[h];
+    if (it && strcmp(it->city, s) == 0) {
+        found = 1;
+        free(it->city);
+        t->l[h] = it->next;
+        free(it);
+    }
+    else {
+        while (it->next) {
+            if (strcmp(it->next->city, s) == 0) {
+                node_t *n = it->next;
+                free(it->next->city);
+                it->next = it->next->next;
+                free(n);
+
+                found = 1;
+                break;
+            }
+            it = it->next;
+        }
+    }
+    if (found == 0) {
+        fprintf(stderr, "not found\n");
+    }
+}
+
+void list(table_t const *t)
+{
+    for (size_t i = 0; i < t->msize; i++) {
+        node_t *it = t->l[i];
+        while (it) {
+            printf("%s %zu\n", it->city, it->p);
+            it = it->next;
+        }
+    }
+}
+
+void init(table_t *t)
+{
+    t->l = calloc(MAX_SIZE, sizeof(node_t *));
+    t->msize = MAX_SIZE;
+}
+
+void destroy(table_t *t)
+{
+    for (size_t i = 0; i < t->msize; i++) {
+        while (t->l[i]) {
+            node_t *n = t->l[i];
+            t->l[i] = t->l[i]->next;
+            free(n->city);
+            free(n);
+        }
+    }
+    free(t->l);
+    t->l = NULL;
+    t->msize = 0;
+}
+
+int main(void)
+{
+    table_t t;
+    init(&t);
+    char l[128] = {0};
+    size_t p = 0;
+    for (;;) {
+        char *m[] = {"exit", "add", "erase", "list"};
+        for (size_t i = 0; i < sizeof(m) / sizeof(m[0]); i++) {
+            printf("%zu - %s\n", i, m[i]);
+        }
+        int ch;
+        if (scanf("%d", &ch) < 1) {
+            break;
+        }
+        if (ch == 0) {
+            break;
+        }
+        else if (ch == 1) {
+            scanf("%s %zu", l, &p);
+            add(&t, l, p);
+        }
+        else if (ch == 2) {
+            scanf("%s", l);
+            erase(&t, l);
+        }
+        else {
+            list(&t);
